855573963e1fbef9e8a022a9fced7aee2df873d0
Anthony Graniero <ananfro@gmail.com>
2019-01-15 20:16:56 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,3 +1,11 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
-  
+    
+    
+  def change
+    add_column :students do |t|
+      t.integer :grade 
+      t.string :birthdate
+ 
+    end
+  end
 end
